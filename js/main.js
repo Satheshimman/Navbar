ffdc72e846893f79fbb1da31d07e0de694e863c0
satheshimman <satheshimman17@gmail.com>
2023-09-29 20:05:29 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var menu=document.getElementById("menu");
+
+console.log("dskdh");
+	 var menulist=document.getElementById("list");
+	 menu.addEventListener("click",()=>{
+	 
+	 
+	     menulist.classList.toggle("showmenu");
+	 
+	 })
